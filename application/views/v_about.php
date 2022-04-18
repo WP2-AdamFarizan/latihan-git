d315cfe93d7cc323065e3130da3a75149a2c5200
WP2-AdamFarizan <farizan.adam@gmail.com>
2022-04-18 11:55:56 +0700

    pertemuan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <img
+ src="https://techstory.in/wp-content/uploads/2022/02/lana-rhoades-fb-1.jpg" alt="Flowers" class="avatar">
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Adam</li>
+ <li>Nama Belakang : Farizan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Benda Barat 11A Blok D/24 No. 13</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bandung</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepakbola</li>
+ <li>Futsal</li>
+ </ul>
+</section>
